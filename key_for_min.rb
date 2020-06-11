d213402255543e51c1befb9826e772de2d7c130f
Grace Tian <gracetian4ever@gmail.com>
2020-06-11 13:45:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = name_hash.each_value[0]
-  name_hash.each_value{|value| puts min_value<< if value < min_value}
+  min_value = nil 
+  min_key = nil 
+  name_hash.each do |key,value|
+    if  value = nil || value < min_value
+      min_value = value
+    end 
   
 end
    
