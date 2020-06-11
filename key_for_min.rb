9ea976288adc56844c0ee781811189ce6a48f0dd
Grace Tian <gracetian4ever@gmail.com>
2020-06-11 13:42:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = name_hash.each_value[0]
+  name_hash.each_value{|value| puts min_value<< if value < min_value}
+  
 end
+   
